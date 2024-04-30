0f641a101541d413bcca4e5d31ca29ec83baf851
martinblot29 <martin.blot@espci.fr>
2024-04-30 20:09:36 +0200

    Update CNN

--- models/CNN_basic.py
+++ models/CNN_basic.py
@@ -12,32 +12,26 @@ class SimplifiedCNN(nn.Module):
             nn.Conv1d(in_channels=self.channels, out_channels=16, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm1d(16),
             nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2, stride=2),
+            nn.MaxPool1d(kernel_size=3, stride=2),
             nn.Dropout(p=self.dropout_prob),
             nn.Conv1d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm1d(32),
             nn.ReLU(),
-            nn.MaxPool1d(kernel_size=4, stride=2),
+            nn.MaxPool1d(kernel_size=3, stride=2),
             nn.Dropout(p=self.dropout_prob),
             nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm1d(64),
             nn.ReLU(),
-            nn.MaxPool1d(kernel_size=4, stride=2),
-            nn.Dropout(p=self.dropout_prob),
-            nn.Conv1d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm1d(128),
-            nn.ReLU(),
-            nn.MaxPool1d(kernel_size=4, stride=2),
+            nn.MaxPool1d(kernel_size=3, stride=2),
             nn.Dropout(p=self.dropout_prob)
         )
         
         # Linear layer
-        self.fc = nn.Linear(128, 5)
+        self.fc = nn.Linear(64*74, 5)
     
     def forward(self, x):
         # Ajouter une dimension de canal
         x = x.unsqueeze(1)
-        
         # Forward pass through convolutional block
         x = self.conv_block(x)
         
